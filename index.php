265fddf08b3be55325ce933fd21c75f1f991f22a
Anne Hazel <anne.hazel2@gmail.com>
2017-02-13 19:09:24 -0500

    added png file and basic css

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 
 <html>
@@ -9,10 +10,33 @@
 </head>
 
 <body>
-    
+
+<div id="main">  
     <h1>Anne Hazel</h1>
-    <img scr="#" alt="picture of Anne Hazel"/>
+    <img id = "headshot" src="images/hazel-anne.png" alt="picture of Anne Hazel"/>
+    
     <p>Anne Hazel</p>
+
+    <!-- start PHP random quote
+    Quotes taken from http://spanish-phrases.com/spanish-quotes/
+    -->
+    <div>
+        <?php
+            $quoteArray = [
+                0 => 'Hacer de tripas corazón.',
+                1 => 'El que algo quiere, algo le cuesta.',
+                2 => 'En los nidos de antaño, no hay pájaros hogaño.',
+                3 => 'El mejor halago es que lo imiten a uno.',
+                4 => 'A diario una manzana es cosa sana.'
+         ];
+        
+        
+        $quote = 'Final Quote';
+        echo $quote;
+    ?>
+    
+    </div>
+</div> <!--end of #main-->
     
 </body>
 </html>
